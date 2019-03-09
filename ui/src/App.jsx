1cc421fd869ed66b76070fd2f88b61bf3b5651d7
Lucas Polito <lucas.polito@nokia.com>
2019-03-09 09:11:57 -0500

    Add react-player - this pretty much handles all my current use cases for the player. Includes a onProgress callback which is perfect.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,61 +1,51 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import {Global, keyframes} from '@emotion/core';
+import {Global} from '@emotion/core';
+
+import ReactPlayer from 'react-player';
 
 import {globalStyles} from './global-styles';
 
-import logo from './logo.svg';
-
 const AppBody = styled.div`
-  text-align: center;
+    text-align: center;
 `;
 
-const AppLogoKeyAnimations = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
+const AppContent = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
 `;
 
-const AppLogo = styled.img`
-  animation: ${AppLogoKeyAnimations} infinite 20s linear;
-  height: 40vmin;
-  pointer-events: none;
-`;
-
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-const AppLink = styled.a`
-  color: #61dafb;
-`;
+const VIDEOS = [
+    'https://www.youtube.com/watch?v=6Y0WE625Mo4',
+    'https://www.youtube.com/watch?v=FJokA_4L5sk',
+    'https://www.youtube.com/watch?v=SYO_uzvATJc',
+];
 
 
-export const App = () => (
-    <AppBody>
-        <Global styles={globalStyles} />
-        <AppHeader>
-            <AppLogo src={logo} alt="logo" />
-            <p>
-                Edit <code>src/App.jsx</code> and save to reload.
-            </p>
-            <AppLink
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </AppLink>
-        </AppHeader>
-    </AppBody>
-);
+export const App = () => {
+    const [video, setVideo] = React.useState(VIDEOS[0]);
+
+    return (
+        <AppBody>
+            <Global styles={globalStyles} />
+            <AppContent>
+                <ReactPlayer
+                    url={video}
+                    playing
+                    onProgress={(prog) => console.log(prog)}
+                />
+                <select onChange={(e) => setVideo(e.target.value)}>
+                    {VIDEOS.map((v) => (
+                        <option key={v} value={v}>{v}</option>
+                    ))}
+                </select>
+            </AppContent>
+        </AppBody>
+    );
+};
