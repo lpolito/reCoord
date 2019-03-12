bb373e1f9f77e7f7c087126e206eeba5c444712f
Lucas Polito <lucas.polito@nokia.com>
2019-03-12 13:14:06 -0400

    Mock coord + timeline that's meh

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -5,24 +5,51 @@ import {Global} from '@emotion/core';
 import ReactPlayer from 'react-player';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import {grey} from '@material-ui/core/colors';
 
 import {ThemeProvider} from './theme';
 
 import {globalStyles} from './global-styles';
 
+const calculatePosition = ({length, xPosition}) => (xPosition / length) * 100;
+
 const AppBody = styled.div`
     text-align: center;
 `;
 
-const AppContent = styled.header`
+const AppContent = styled.div`
     background-color: #282c34;
     min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
+`;
+
+const Timeline = styled.div`
+    background-color: black;
+
+    display: flex;
+    flex-direction: column;
+
+    width: ${({length}) => `${length}px`};
+    overflow-y: auto;
+    padding: 4px;
+`;
+
+const Clip = styled.div`
+    width: ${({duration}) => `${duration}px`};
+    margin: 2px;
+    margin-left: ${((args) => `${calculatePosition(args)}%`)};
+
+    background-color: ${grey[100]};
+    color: ${grey[800]};
+    padding: 5px;
+    border-radius: 4px;
+    font-size: 12px;
+    white-space: nowrap; 
+    overflow: hidden;
+    text-overflow: ellipsis;
 `;
 
 const VIDEOS = [
@@ -31,9 +58,59 @@ const VIDEOS = [
     'https://www.youtube.com/watch?v=SYO_uzvATJc',
 ];
 
+const COORD = {
+    id: 1,
+    name: '',
+    length: 500,
+    clips: [{
+        url: 'https://www.youtube.com/watch?v=6Y0WE625Mo4',
+        duration: 194,
+        title: '1', // Gorillaz - Superfast Jellyfish (Live in Detroit 2017)',
+        thumbnails: {},
+        xPosition: 0,
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=FJokA_4L5sk',
+        duration: 159,
+        title: '2', // 'Gorillaz - \'Superfast Jellyfish\' @ Fox theatre, Detroit 2017',
+        thumbnails: {},
+        xPosition: 100,
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=SYO_uzvATJc',
+        duration: 22,
+        title: '3', // 'Gorillaz - Superfast Jellfish live in Detroit',
+        thumbnails: {},
+        xPosition: 250,
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=6Y0WE625Mo4',
+        duration: 50,
+        title: '4', // 'Gorillaz - Superfast Jellyfish (Live in Detroit 2017)',
+        thumbnails: {},
+        xPosition: 50,
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=FJokA_4L5sk',
+        duration: 175,
+        title: '5', // 'Gorillaz - \'Superfast Jellyfish\' @ Fox theatre, Detroit 2017',
+        thumbnails: {},
+        xPosition: 300,
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=SYO_uzvATJc',
+        duration: 320,
+        title: '6', // 'Gorillaz - Superfast Jellfish live in Detroit',
+        thumbnails: {},
+        xPosition: 400,
+    }],
+};
+
 
 export const App = () => {
     const [video, setVideo] = React.useState(VIDEOS[0]);
+
+    const {length, clips} = COORD;
 
     return (
         <ThemeProvider>
@@ -42,7 +119,7 @@ export const App = () => {
                 <AppContent>
                     <ReactPlayer
                         url={video}
-                        playing
+                        // playing
                         onProgress={(prog) => console.log(prog)}
                     />
                     <Select value={video} onChange={(e) => setVideo(e.target.value)}>
@@ -50,6 +127,19 @@ export const App = () => {
                             <MenuItem key={v} value={v}>{v}</MenuItem>
                         ))}
                     </Select>
+
+                    <Timeline length={length}>
+                        {clips.map(({duration, xPosition, title}) => (
+                            <Clip
+                                duration={duration}
+                                length={length}
+                                xPosition={xPosition}
+                            >
+                                {title}
+                            </Clip>
+                        ))}
+                    </Timeline>
+
                 </AppContent>
             </AppBody>
         </ThemeProvider>
