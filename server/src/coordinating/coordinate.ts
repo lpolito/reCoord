90091ea9f2dd41b6de6d665e1cf3d17b85cf8773
Lucas Polito <lucas.polito@nokia.com>
2019-09-16 11:17:31 -0400

    Make the old server behave like the new one. This doesn't really give me what I want but I know how to read it.

--- server/src/coordinating/coordinate.ts
+++ server/src/coordinating/coordinate.ts
@@ -13,81 +13,103 @@ interface FingerprintData {
     duration: number;
 }
 
+const zip = (a: any[], b: any[]) => (
+    a.map((e, i) => [e, b[i]])
+);
+
+
+const returnMatches = (hashes: number[][], idComparing: string, hashesToCheck: number[][]) => {
+    const results: number[][] = [];
+    const hashMap: {[key: number]: number} = {};
+
+    hashes.forEach(([hash, offset]) => {
+        hashMap[hash] = offset;
+    });
+
+    hashesToCheck.forEach(([hash, offset]) => {
+        if (hashMap[hash]) {
+            results.push([Number.parseInt(idComparing, 10), offset - hashMap[hash]]);
+        }
+    });
+
+    return results;
+};
+
+const findMatches = (fingerprintsById: any) => {
+    const matchesById: any = {};
+
+    Object.keys(fingerprintsById).forEach((id) => {
+        const hashes = fingerprintsById[id];
+
+        // Avoid comparing video to itself.
+        Object.keys(fingerprintsById).forEach((idComparing) => {
+            if (idComparing === id) return;
+
+            const hashesToCheck = fingerprintsById[idComparing];
+            matchesById[id] = returnMatches(hashes, idComparing, hashesToCheck);
+        });
+    });
+
+    return matchesById;
+};
+
+const alignMatches = (matches: any) => {
+    const diffCounter: {[key: number]: any} = {};
+    let largest = 0;
+    let largestCount = 0;
+    let matchId = -1;
+
+    matches.forEach((tup: number[]) => {
+        const [sid, diff] = tup;
+
+        if (!diffCounter[diff]) {
+            diffCounter[diff] = {};
+        }
+
+        if (!diffCounter[diff][sid]) {
+            diffCounter[diff][sid] = 0;
+        }
+
+        diffCounter[diff][sid] += 1;
+
+        if (diffCounter[diff][sid] > largestCount) {
+            largest = diff;
+            largestCount = diffCounter[diff][sid];
+            matchId = sid;
+        }
+    });
+
+    const FFT = 512;
+    const STEP = FFT / 2;
+    const DT = 1 / (22050 / STEP);
+
+    const nseconds = (DT * largest) / 1000;
+
+    return {
+        diffCounter,
+        confidence: largestCount,
+        offset: largest,
+        offsetSeconds: nseconds,
+        matchId,
+    };
+};
+
 
 export const syncByBuckets = () => (
     new Promise(async (resolve, reject) => {
-        const fpDatas: FingerprintData[] = [
-            {
-                id: 1,
-                buffer: buffer1,
-                duration: 194,
-            },
-            {
-                id: 2,
-                buffer: buffer2,
-                duration: 159,
-            },
-            {
-                id: 3,
-                buffer: buffer3,
-                duration: 22,
-            },
-        ];
+        const foo: any = {
+            1: zip(buffer1.hcodes, buffer1.tcodes),
+            2: zip(buffer2.hcodes, buffer2.tcodes),
+            3: zip(buffer3.hcodes, buffer3.tcodes),
+        };
 
-        fpDatas.forEach((curFpData) => {
-            const {
-                id: curId,
-                buffer: {
-                    tcodes: curTCodes,
-                    hcodes: curHCodes,
-                },
-            } = curFpData;
+        const matchesById = findMatches(foo);
 
-            fpDatas.forEach((fpDataToCheck) => {
-                // Skip over the current fingerprint data.
-                if (curId === fpDataToCheck.id) return;
-
-                const {
-                    id: idToCheck,
-                    buffer: {
-                        tcodes: tcodesToCheck,
-                        hcodes: hcodesToCheck,
-                    },
-                } = fpDataToCheck;
-
-                const matchingTCodes: number[] = [];
-                const matchingHCodes: number[] = [];
-
-                tcodesToCheck.forEach((t, i) => {
-                    const hcodeToCheck = hcodesToCheck[i];
-
-                    // Get all matching indices of hcodes.
-                    const matchIndices: number[] = [];
-                    let x = curHCodes.indexOf(hcodeToCheck);
-                    while (x !== -1) {
-                        matchIndices.push(x);
-                        x = curHCodes.indexOf(hcodeToCheck, x + 1);
-                    }
-
-                    // No matches.
-                    if (matchIndices.length === 0) return;
-
-                    // const matchingTCodes: number[] = [];
-                    matchIndices.forEach((index) => {
-                        matchingTCodes.push(curTCodes[index]);
-                        matchingHCodes.push(curHCodes[index]);
-                    });
-                });
-
-                console.log({
-                    curId,
-                    idToCheck,
-                    matchingTCodes,
-                    matchingHCodes,
-                });
-            });
+        const result: any = {};
+        Object.keys(matchesById).forEach((id) => {
+            result[id] = alignMatches(matchesById[id]);
         });
 
-        resolve({});
+        resolve(result);
     })
 );
