d715004aecf900ba66c98797e24153b9a8a0b971
Lucas Polito <lucas.polito@nokia.com>
2019-09-23 16:02:15 -0400

    Delete node server - RIP

--- server/src/fingerprinting/types.ts
+++
@@ -1,4 +0,0 @@
-export interface FingerprintBuffer {
-    tcodes: number[];
-    hcodes: number[];
-}
--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -5,20 +5,32 @@ module.exports = {
     "parser": "@typescript-eslint/parser",
     "plugins": ["@typescript-eslint", "emotion", "react-hooks"],
     "rules": {
-        ...parentConfig.rules,
+        "indent": ["error", 4],
+        "import/prefer-default-export": 0,
+        "object-curly-spacing": [2, "never"],
+        "arrow-parens":["error", "always"],
+        "prefer-destructuring": 0,
+        "max-len": ["error", { "code": 120}],
+        "comma-dangle": ["error", {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "always-multiline",
+            "exports": "always-multiline",
+            "functions": "never"
+        }],
 
         // React
         "jsx-a11y/click-events-have-key-events": 0,
         "react-hooks/rules-of-hooks": ["error"],
-        "react/jsx-indent": parentConfig.rules.indent,
-        "react/jsx-indent-props": parentConfig.rules.indent,
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
         "react/jsx-one-expression-per-line": 0,
         "react/jsx-filename-extension": [1, {extensions: [".jsx", ".tsx"]}],
         "jsx-quotes":["error", "prefer-single"],
         "react/prop-types": 0,
 
         // Typescript
-        "@typescript-eslint/indent": [...parentConfig.rules.indent, {SwitchCase: 0}],
+        "@typescript-eslint/indent": [["error", 4], {SwitchCase: 0}],
         "@typescript-eslint/explicit-function-return-type": 0,
         "@typescript-eslint/no-non-null-assertion": 0,
         "@typescript-eslint/no-object-literal-type-assertion": 0, // Try to get around this
